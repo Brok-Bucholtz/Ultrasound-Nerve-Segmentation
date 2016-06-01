123b6d52eca7e27b32be0290c9e8c746c7fdb64b
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-06-01 13:25:20 -0500

    Normalize image features

--- feature_extraction.py
+++ feature_extraction.py
@@ -10,6 +10,10 @@ def _get_file_root_name(file_path):
     return file_root_name_expression.search(file_path).group(0)[1:-1]
 
 
+def _normalization_pixel(pixel):
+    return pixel/255.0
+
+
 def _get_feature_label_images():
     TRAIN_FILES = './data/train/*.tif'
     mask_expression = re.compile(r'_mask\.tif', re.IGNORECASE)
@@ -24,9 +28,9 @@ def _get_feature_label_images():
                 root_name = _get_file_root_name(file_name)
 
                 if mask_expression.search(file_name):
-                    labels[root_name[:-5]] = image.getdata()
+                    labels[root_name[:-5]] = list(image.getdata())
                 else:
-                    features[root_name] = image.getdata()
+                    features[root_name] = [_normalization_pixel(pixel) for pixel in image.getdata()]
 
     assert len(features) == len(labels)
     return [(features[root_name], labels[root_name]) for root_name, label in labels.items()]
@@ -40,7 +44,7 @@ def get_detection_data():
     with tqdm(desc='Extracting Features', total=len(features_labels), unit='image') as progress_bar:
         for feature, label in features_labels:
             progress_bar.update()
-            features.append(list(feature))
+            features.append(feature)
             labels.append(255 in label)
 
     return features, labels
