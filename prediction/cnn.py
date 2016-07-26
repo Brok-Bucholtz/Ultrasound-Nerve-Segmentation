733bdd80623fc881d441b0a4912c26a134fcc498
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-26 09:51:55 -0700

    Move CNN run function to run module

--- prediction/cnn.py
+++ prediction/cnn.py
@@ -1,10 +1,4 @@
 import tensorflow as tf
-import cv2
-import math
-from sklearn import cross_validation
-from sklearn.metrics import f1_score
-
-from feature_extraction import get_detection_data
 
 
 def _create_layer(layer_input, weight, bias):
@@ -18,7 +12,7 @@ def _create_layer(layer_input, weight, bias):
         padding='SAME')
 
 
-def _create_cnn(model_input, model_output, dropout, image_shape, n_classes):
+def create_cnn(model_input, model_output, dropout, image_shape, n_classes):
     learning_rate = 0.001
 
     model_input = tf.reshape(model_input, shape=[-1, image_shape[0], image_shape[1], 1])
@@ -68,42 +62,3 @@ def get_predictions(session, model, model_input, inputs):
     for input in inputs:
         outputs.append(session.run(model, feed_dict={model_input: [input]})[0])
     return outputs
-
-
-def run_cnn_detection():
-    batch_size = 16
-    image_width = 58  # 580
-    image_height = 42  # 420
-    n_classes = 2
-    dropout = 0.75
-
-    model_input = tf.placeholder(tf.float32, [None, image_width, image_height])
-    model_output = tf.placeholder(tf.float32, [None, n_classes])
-    keep_prob = tf.placeholder(tf.float32)
-
-    x_all, y_all = get_detection_data()
-    x_all = [cv2.resize(image, (image_height, image_width), interpolation=cv2.INTER_CUBIC) for image in x_all]
-    y_all = [[float(y_element), float(not y_element)] for y_element in y_all]
-
-    x_train, x_test, y_train, y_test = cross_validation.train_test_split(x_all, y_all, test_size=0.25)
-
-    prediction, optimizer = _create_cnn(model_input, model_output, dropout, (image_width, image_height), n_classes)
-
-    with tf.Session() as sess:
-        sess.run(tf.initialize_all_variables())
-
-        print "Training CNN..."
-        for batch_i in xrange(int(math.ceil(len(x_train)/float(batch_size)))):
-            front_batch = batch_i * batch_size
-            batch_x = x_train[front_batch: front_batch + batch_size]
-            batch_y = y_train[front_batch: front_batch + batch_size]
-
-            sess.run(optimizer, feed_dict={model_input: batch_x, model_output: batch_y, keep_prob: dropout})
-
-        print "F1 score for train set: {}".format(f1_score(
-            [y[1] for y in y_train],
-            get_predictions(sess, prediction, model_input, x_train)))
-        print "Predicting Test Set..."
-        print "F1 score for test set: {}".format(f1_score(
-            [y[1] for y in y_test],
-            get_predictions(sess, prediction, model_input, x_test)))
--- prediction/run.py
+++ prediction/run.py
@@ -1,9 +1,13 @@
+import tensorflow as tf
+import cv2
 from sklearn import cross_validation
 from sklearn.metrics import f1_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
+from math import ceil
+
 from feature_extraction import get_detection_data
-from prediction.cnn import run_cnn_detection
+from prediction.cnn import create_cnn, get_predictions
 
 
 def _run_knn_detection():
@@ -30,6 +34,44 @@ def _run_svm_detection():
     print "F1 score for training set: {}".format(f1_score(y_train, clf.predict(x_train)))
     print "Predicting Test Set..."
     print "F1 score for test set: {}".format(f1_score(y_test, clf.predict(x_test)))
+
+
+def run_cnn_detection():
+    batch_size = 16
+    image_width = 58  # 580
+    image_height = 42  # 420
+    n_classes = 2
+    dropout = 0.75
+
+    model_input = tf.placeholder(tf.float32, [None, image_width, image_height])
+    model_output = tf.placeholder(tf.float32, [None, n_classes])
+    keep_prob = tf.placeholder(tf.float32)
+
+    x_all, y_all = get_detection_data()
+    x_all = [cv2.resize(image, (image_height, image_width), interpolation=cv2.INTER_CUBIC) for image in x_all]
+    y_all = [[float(y_element), float(not y_element)] for y_element in y_all]
+
+    x_train, x_test, y_train, y_test = cross_validation.train_test_split(x_all, y_all, test_size=0.25)
+
+    prediction, optimizer = create_cnn(model_input, model_output, dropout, (image_width, image_height), n_classes)
+
+    with tf.Session() as sess:
+        sess.run(tf.initialize_all_variables())
+
+        print "Training CNN..."
+        for batch_i in xrange(int(ceil(len(x_train)/float(batch_size)))):
+            front_batch = batch_i * batch_size
+            batch_x = x_train[front_batch: front_batch + batch_size]
+            batch_y = y_train[front_batch: front_batch + batch_size]
+
+            sess.run(optimizer, feed_dict={model_input: batch_x, model_output: batch_y, keep_prob: dropout})
+
+        print "F1 score for train set: {}".format(f1_score(
+            [y[1] for y in y_train],
+            get_predictions(sess, prediction, model_input, x_train)))
+        print "F1 score for test set: {}".format(f1_score(
+            [y[1] for y in y_test],
+            get_predictions(sess, prediction, model_input, x_test)))
 
 
 if __name__ == '__main__':
