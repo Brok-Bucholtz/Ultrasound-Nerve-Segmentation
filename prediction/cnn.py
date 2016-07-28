58850417fee0875ecb1eae34bdad4ed480347997
Brok Bucholtz <brok.bucholtz@gmail.com>
2016-07-28 13:53:31 -0700

    Rename function to better reflect what is does

--- prediction/cnn.py
+++ prediction/cnn.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 
 
-def _create_layer(layer_input, weight, bias):
+def _create_maxpool_layer(layer_input, weight, bias):
     layer = tf.nn.conv2d(layer_input, weight, strides=[1, 1, 1, 1], padding='SAME')
     layer = tf.nn.bias_add(layer, bias)
     layer = tf.nn.relu(layer)
@@ -22,23 +22,23 @@ def create_cnn(model_input, dropout, image_shape, resize_dividend, n_classes):
         tf.image.ResizeMethod.BICUBIC)
     tf.image_summary('Resize', model_input)
 
-    conv1 = _create_layer(
+    conv1 = _create_maxpool_layer(
         model_input,
         tf.Variable(tf.random_normal([3, 3, 1, 16])),
         tf.Variable(tf.random_normal([16])))
-    conv2 = _create_layer(
+    conv2 = _create_maxpool_layer(
         conv1,
         tf.Variable(tf.random_normal([3, 3, 16, 32])),
         tf.Variable(tf.random_normal([32])))
-    conv3 = _create_layer(
+    conv3 = _create_maxpool_layer(
         conv2,
         tf.Variable(tf.random_normal([3, 3, 32, 64])),
         tf.Variable(tf.random_normal([64])))
-    conv4 = _create_layer(
+    conv4 = _create_maxpool_layer(
         conv3,
         tf.Variable(tf.random_normal([3, 3, 64, 128])),
         tf.Variable(tf.random_normal([128])))
-    conv5 = _create_layer(
+    conv5 = _create_maxpool_layer(
         conv4,
         tf.Variable(tf.random_normal([3, 3, 128, 256])),
         tf.Variable(tf.random_normal([256])))
